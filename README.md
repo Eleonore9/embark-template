f533fb6c6a46917316d5c7debcf7a7fd698cb2f4
mj <inchingforward@gmail.com>
2014-08-05 13:53:11 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-embark
-======
+# embark
 
-A simple Clojure+ClojureScript starter project
+A Leiningen template for creating a simple Clojure+ClojureScript project.
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2014 FIXME
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject embark/lein-template "0.1.0-SNAPSHOT"
+  :description "A simple Clojure+ClojureScript starter project."
+  :url "http://github.com/inchingforward/embark"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :eval-in-leiningen true)
