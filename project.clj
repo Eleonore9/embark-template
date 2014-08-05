3dbe868a9b2918a9ad1af5eb81de75b48466ed14
mj <inchingforward@gmail.com>
2014-08-05 14:46:19 -0500

    Changed license.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject embark/lein-template "0.1.0-SNAPSHOT"
   :description "A simple Clojure+ClojureScript starter project."
   :url "http://github.com/inchingforward/embark"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/MIT"}
   :eval-in-leiningen true)
