86260b2f6926dac547cb8893fe47f7c5c930c4aa
inchingforward <inchingforward@gmail.com>
2014-08-05 20:25:36 -0500

    Removed unused file.

--- src/leiningen/new/embark.clj
+++ src/leiningen/new/embark.clj
@@ -9,7 +9,6 @@
   (let [data {:name name
               :sanitized (name-to-path name)}]
     (->files data
-             ["src/{{sanitized}}/foo.clj" (render "foo.clj" data)]
              ["clj/src/{{sanitized}}/handler.clj" (render "handler.clj" data)]
              ["cljs/src/{{sanitized}}/core.cljs" (render "core.cljs" data)]
              ["cljs/test/{{sanitized}}/core_test.cljs" (render "core_test.cljs" data)]
